26e9e0fe636dea9648c88a8fdb57ea7ebcabac05
sujpa <sujpa@TRI-NB0650.tricentis.com>
2022-03-10 23:08:47 +0530

    first invoke browser commit

---
+++ selenium-start/src/selintro.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class selintro {
+	
+	public static void main(String[] args) {
+		
+		//Invoking the browser
+		
+		System.setProperty("webdriver.chrome.driver","/Users/sujpa/eclipse-workspace/selenium-start/chromedriver/chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver(); //ChromeDriver driver = new ChromeDriver();
+		driver.get("https://sujpa.qtestnet.com");
+		driver.getTitle();
+		driver.getCurrentUrl();
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		
+		driver.close();
+	}
+}
